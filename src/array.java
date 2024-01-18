a85939fa3da8fc55986e3f9be59eec6fd195f334
sahil-vasyerp <sahilmahyavanshi@vasyerp.com>
2024-01-18 11:11:21 +0530

    basic java-array

--- src/array.java
+++ src/array.java
@@ -5,12 +5,39 @@ public class array {
 
         System.out.println(name[0]);
 
-//        output by for loop
+//        Output by for loop
         for (String i :name)
         {
             System.out.println("My name is "+i);
         }
 
-//
+//          Changes in array
+
+        name[0]="ahishek";
+        System.out.println(name[0]);
+
+//        check array length
+        System.out.println(name.length);
+
+//        loop through array
+
+        String[] cars={"bmw","audi","range-rover","verna","swift"};
+
+        for (int i=0;i< cars.length;i++)
+        {
+            System.out.println("My favourite car is "+ cars[i]  );
+        }
+
+//        multi-dimensional Array
+
+        int[][] myNum={{2,1,3,5},{5,6,8,9,7}};
+
+        System.out.println(myNum[0][2]);
+        System.out.println(myNum[0][3]+myNum[1][2]);
+
+        for (int i:myNum[1])
+        {
+            System.out.println(i);
+        }
     }
 }
